d4afc590ec896b455a540903c90d2b57ad1ee2ce
Jason Shulmeister <jason@coloradocareassist.com>
2025-12-04 21:53:48 -0700

    Add missing pydantic import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, Request, Depends, status, Query
+from pydantic import BaseModel
 from fastapi.responses import HTMLResponse, JSONResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
